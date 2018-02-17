a99d0f45c6e2df019cde8fb531e35ccb68317537
Prashant C <prashant.c@cisinlabs.com>
2018-02-17 20:46:48 +0530

    remove unused code from mcphotonslib.py

--- mcphotonslib.py
+++ mcphotonslib.py
@@ -1,24 +1,21 @@
-freePath ='/usr/lib/freecad/lib'
-import numpy, os, sys
+
+from PySide import QtCore
+
+import os
+import sys
+
+freePath = '/usr/lib/freecad/lib'
 sys.path.append(freePath)
-import FreeCAD
-import FreeCADGui
-import Part
-from PySide import QtGui, QtCore
-from lib3D import fit_plane_to_surface1, fit_rotation_axis_to_surface1
-from viewProviderProxies import ImportedPartViewProviderProxy, ConstraintViewProviderProxy,\
-     create_constraint_mirror, repair_tree_view
 
 path_mcphotons = os.path.dirname(__file__)
-#path_assembly2_icons =  os.path.join( path_assembly2, 'Resources', 'icons')
-#path_assembly2_ui =  os.path.join( path_assembly2, 'Resources', 'ui')
-path_mcphotons_resources = os.path.join( path_mcphotons, 'Gui', 'Resources', 'resources.rcc')
+path_mcphotons_resources = os.path.join(
+    path_mcphotons, 'Gui', 'Resources', 'resources.rcc')
 resourcesLoaded = QtCore.QResource.registerResource(path_mcphotons_resources)
 assert resourcesLoaded
-#update resources file using 
-# $rcc -binary  Gui/Resources/resources.qrc -o Gui/Resources/resources.rcc 
+# update resources file using
+# $rcc -binary  Gui/Resources/resources.qrc -o Gui/Resources/resources.rcc
 
 __dir__ = path_mcphotons
 wb_globals = {}
 __dir2__ = os.path.dirname(__file__)
-GuiPath = os.path.expanduser ("~") #GuiPath = os.path.join( __dir2__, 'Gui' )
+GuiPath = os.path.expanduser("~")  # GuiPath = os.path.join( __dir2__, 'Gui' )
